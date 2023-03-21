96845ee4b7b76909af854aaaad129591e2027671
doorlck <lsoholee0113@gmail.com>
2023-03-21 20:54:16 +0900

    message

---
+++ lab2_2.py
@@ -0,0 +1,15 @@
+x= 10.3 
+y= 7
+u= x*y
+v= x/y
+w= x//y
+z= x % y
+a= 10.65 / x*(100-y)**z
+print("x*y=",u,"\nx/y=",v,"\nx//y=",w,"\nx%y=",z,"\na=",a)
+
+#우선순위(precedence rule)
+#parenthesis 괄호
+#exp
+#mul,div,mod
+#add,sub
+#left to right
