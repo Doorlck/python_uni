0ba32a1ac6821cf1f84d2b67ab8e173ce85bf75d
doorlck <lsoholee0113@gmail.com>
2023-03-22 11:37:55 +0900

    first command

---
+++ lab2_3.py
@@ -0,0 +1,11 @@
+x= "Users>jomia>python"
+y= "string oprators are unique"
+#1. concatenation
+z=x+y
+print(x,"+",y,"=",z)
+#repetition
+z=x*2
+print(x,"*2=",z)
+#slicing
+w=x[0]
+print(x,"[1]=",w,sep="")
